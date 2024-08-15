ee243f106606f73dec311c071fec1a5f0f0819a2
Niraj <nirajnrjg@gmail.com>
2024-08-15 07:09:12 +0530

    Read the data from the excel file

--- src/test/java/TestCases/BaseClass.java
+++ src/test/java/TestCases/BaseClass.java
@@ -1,15 +1,23 @@
 package TestCases;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BaseClass
 {
    public static WebDriver driver;
+ public XSSFWorkbook wbook;
+ public XSSFSheet sheet;
 
     @BeforeMethod
     public void setUpDriver()
@@ -25,5 +33,22 @@ public class BaseClass
     {
         driver.quit();
     }
+ @BeforeTest
+ public void SetUpExcel() throws IOException
+ {
+
+  FileInputStream fis = new FileInputStream("exceldata.xlsx");
+  wbook = new XSSFWorkbook(fis);
+  sheet = wbook.getSheet("Sheet1");
+
+ }
+
+ @AfterTest
+ public void CloseExcel() throws IOException
+ {
+  wbook.close();
+ }
+
+
 
 }
--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -7,9 +7,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class LoginTest extends BaseClass
 {
@@ -17,7 +21,7 @@ public class LoginTest extends BaseClass
     public void TC01_LoginFailrueTest() {
 
         LoginPage loginPage = new LoginPage();
-        loginPage.LoginFunction("nirajnrjg@gmail.com", "ABC@123");
+        loginPage.LoginFunction("abx@xyz.com", "Abc@1234");
         loginPage.ValidateErrorMsg("The email or password you have entered is invalid.");
     }
     @Test
@@ -26,6 +30,47 @@ public class LoginTest extends BaseClass
         LoginPage loginPage=new LoginPage();
         loginPage.LoginFunction("nirajnrjg@gmail.com","Simpli@123");
 
+    }
+@Test
+@Parameters({"param1","param2"})
+    public void TC03_LoginFailrueTest(String uname, String  pwd)
+    {
+        LoginPage loginPage=new LoginPage();
+       // loginPage.LoginFunction("nirajnrjg@gmail.com","Simpli@123");
+         loginPage.LoginFunction(uname, pwd);
+        loginPage.ValidateErrorMsg("The email or password you have entered is invalid.");
+
+    }
+    @Test(dataProvider = "dp")
+    public void TC04_LoginFailrueTest(String key)
+    {
+        LoginPage loginPage=new LoginPage();
+        // loginPage.LoginFunction("nirajnrjg@gmail.com","Simpli@123");
+        loginPage.LoginFunction(key,testdata.get(key));
+        loginPage.ValidateErrorMsg("The email or password you have entered is invalid.");
+
+    }
+    @Test
+    public void TC05_LoginFailrueTest_ExcelData()
+    {
+
+        LoginPage loginPage = new LoginPage();
+        String userval= sheet.getRow(1).getCell(0).getStringCellValue();
+        String passwordval= sheet.getRow(1).getCell(1).getStringCellValue();
+        loginPage.LoginFunction(userval,passwordval) ;
+        loginPage.ValidateErrorMsg("The email or password you have entered is invalid.");
+    }
+
+
+
+    Map<String,String> testdata =new HashMap<String,String>();
+    @DataProvider (name="dp")
+    public Iterator<String> method1()
+    {
+        testdata.put("aaa@xyz.com","Abc@123");
+        testdata.put("bbb@xyz.com","Abc@1234");
+        testdata.put("ccc@xyz.com","Abc@1235");
+        return  testdata.keySet().iterator();
     }
 
 }
